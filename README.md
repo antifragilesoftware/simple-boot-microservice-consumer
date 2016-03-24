958213a4771eb8aa39ca21132b12af17d32d3584
Russ Miles <russ@russmiles.com>
2016-03-24 18:00:53 +0000

    First configured version of this service

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# simple-boot-microservice-consumer
-A Spring Boot Microservice that uses and therefore depends on another microservice
+# Sample Spring Boot Service that consumes another service through Consul
+
+## To Build into Docker using Maven
+
+First make sure you're running in an environment that has docker available to you. Then execute:
+
+$ mvn package docker:build
+
+Once completed you will have an atomist/simple-boot-microservice-consumer image available, as seen by executing:
+
+$ docker images
+
+```
+REPOSITORY                                                      TAG                 IMAGE ID            CREATED             SIZE
+antifragilesoftware/simple-boot-microservice-consumer                       latest              39fc873649d9        3 seconds ago       667.6 MB
+```
---
+++ src/main/java/com/atomist/samples/SimpleBootMicroserviceApplication.java
@@ -0,0 +1,68 @@
+package com.atomist.samples;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@RestController
+@EnableDiscoveryClient
+@EnableHystrix
+public class SimpleBootMicroserviceApplication {
+
+    @Value("${consumed.microservice.name}")
+    private String consumedServiceName;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/")
+    @HystrixCommand
+    public String home() {
+        return "Simple Boot Microservice Consumer Alive!";
+    }
+
+    @RequestMapping("/discoveredServiceInstances")
+    public String serviceInstances() {
+        StringBuffer discoveredServiceInstances = new StringBuffer();
+
+        discoveryClient.getInstances(consumedServiceName).forEach((ServiceInstance s) -> {
+            discoveredServiceInstances.append("Services Discovered: ").append(ToStringBuilder.reflectionToString(s)).append('\n');
+        });
+
+        return discoveredServiceInstances.toString();
+    }
+
+    @RequestMapping("/invokeConsumedService")
+    public String invokeConsumedService() {
+        ResponseEntity<String> exchange =
+                this.restTemplate.exchange(
+                        "http://microservice1/",
+                        HttpMethod.GET,
+                        null,
+                        String.class);
+
+        return exchange.getBody();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBootMicroserviceApplication.class, args);
+    }
+}
